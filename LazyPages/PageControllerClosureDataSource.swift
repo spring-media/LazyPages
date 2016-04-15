b81d7101800cc74ae38a45c940d1fd9e8c18a95a
Vargas Casaseca, Cesar <cesar.vargas-casaseca@asideas.de>
2016-04-15 10:06:57 +0200

    More ways to provide a data source.

---
+++ LazyPages/PageControllerClosureDataSource.swift
@@ -0,0 +1,27 @@
+//
+//  ClosureDataSource.swift
+//  LazyPages
+//
+//  Created by Vargas Casaseca, Cesar on 15.04.16.
+//  Copyright © 2016 WeltN24. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+/// Use this class when you want to create your view controllers by executing a closure by the right index
+public final class PageControllerClosureDataSource: PageControllerDataSource {
+  private let closures: [() -> UIViewController]
+  
+  public init(closures: [() -> UIViewController]) {
+    self.closures = closures
+  }
+  
+  public func viewControllerAtIndex(index: Int) -> UIViewController {
+    return closures[index]()
+  }
+  
+  public func numberOfViewControllers() -> Int {
+    return closures.count
+  }
+}
